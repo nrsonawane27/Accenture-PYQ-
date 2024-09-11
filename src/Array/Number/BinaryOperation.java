1da5f5a644c2a0cdc823bcf3171066a57e224a3a
nikita sonawane <nrsonawane27@gmail.com>
2024-09-11 22:21:10 +0530

    Binary Operation

---
+++ src/Array/Number/BinaryOperation.java
@@ -0,0 +1,24 @@
+/*
+      3. Write a function Calculate Binary Operations(str) that accepts the string as an
+      argument or parameter. The string should contain the binary numbers with their operators
+       OR, AND, and XOR?
+       a. A Means the AND Operation.
+       b. B Means the OR Operation.
+       c. C Means the XOR Operation.
+       By scanning the given string from left to right you've to calculate the string and
+       by taking one operator at a time then return the desired output.
+       Conditions:
+       1. The priority of the operator is not required
+       2. The length of the string is always Odd
+       3.If the length of the string is null then return -1.
+       Sample Test Case:
+       Input:
+       1C0C1C1A081
+       Output :
+       1
+ */
+package Array.Number;
+
+public class BinaryOperation {
+
+}
